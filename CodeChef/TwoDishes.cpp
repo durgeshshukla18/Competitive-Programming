c78e3fed869cdef45ff8eee99bf63cd990b6d4c9
Durgesh Shukla <thegdshukla@gmail.com>
2024-02-16 15:18:58 +0530

    third commit

--- CodeChef/TwoDishes.cpp
+++ CodeChef/TwoDishes.cpp
@@ -4,33 +4,16 @@ int main(){
     int t;
     cin>>t;
     for(int i=0;i<t;i++){
-        int n,a,b,c;
-        cin>>n>>a>>b>>c;
-        if(a>b){
-            int x=a-b;
-            if(b==n){
-                cout<<"YES";
+        int n,fr,veg,fi;
+        cin>>n>>fr>>veg>>fi;
+        if(veg>=n){
+            if((fr+fi)>=n){
+                cout<<"YES"<<endl;
+            }else{
+                cout<<"NO"<<endl;
             }
-        }
-        else if(b>a){
-            int x=b-a;
-            if(x>c){
-                int y=x-c;
-                int z=a+c;
-                if(z==n){
-                    cout<<"YES";
-                }
-            }
-            if(x<c){
-                int y=c-x;
-                int z=a+x;
-                if(z==n){
-                    cout<<"YES";
-                }
-            }
-        }
-        else{
-            cout<<"NO";
+        }else{
+            cout<<"NO"<<endl;
         }
     }
 }
