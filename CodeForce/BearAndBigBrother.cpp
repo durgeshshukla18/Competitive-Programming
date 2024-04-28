f68dbcf26d4032d77296f43685bf05f1d3f08b28
Durgesh Shukla <thegdshukla@gmail.com>
2024-04-29 02:13:00 +0530

    day seventeen

---
+++ CodeForce/BearAndBigBrother.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int limak,bob;
+    cin>>limak>>bob;
+    int count = 0;
+    while(limak<=bob){
+        limak*=3;
+        bob*=2;
+        count++;
+    }
+    cout<<count;
+}
