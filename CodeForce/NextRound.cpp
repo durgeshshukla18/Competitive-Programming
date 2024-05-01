1402056c6b452b70f4831bfedb9d2fedba1fe89d
Durgesh Shukla <thegdshukla@gmail.com>
2024-05-02 00:07:42 +0530

    day twenty

--- CodeForce/NextRound.cpp
+++ CodeForce/NextRound.cpp
@@ -1,25 +1,34 @@
-//https://codeforces.com/problemset/problem/158/A
+// https://codeforces.com/problemset/problem/158/A
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int numParticipant, finisherPlace;
-    cin>>numParticipant>>finisherPlace;
+    cin >> numParticipant >> finisherPlace;
     int count = 0;
     int arr[numParticipant];
-    for(int i=0;i<numParticipant;i++){
-        int contestant;
-        cin>>contestant;
-        arr[i]=contestant;
-        //if(contestant>finisherPlace) count++;
+    for (int i = 0; i < numParticipant; i++)
+    {
+        // int contestant;
+        // cin>>contestant;
+        cin >> arr[i];
+        // if(contestant>finisherPlace) count++;
     }
-    for(int i=0;i<numParticipant;i++){
-        
-        if(arr[i]>=arr[finisherPlace-1]){
-            if(arr[i]==0) count=count;
-        }else{
-            count++;
+    for (int i = 0; i < numParticipant; i++)
+    {
+
+        if (arr[i] >= arr[finisherPlace - 1])
+        {
+            if (arr[i] == 0)
+            {
+                count = count;
+            }
+            else
+            {
+                count++;
+            }
         }
     }
-    cout<<count;
+        cout << count;
 }
