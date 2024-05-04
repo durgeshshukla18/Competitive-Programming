49a7bb55c23b0866a38c40c54f51e9345588b672
Durgesh Shukla <thegdshukla@gmail.com>
2024-05-05 03:14:46 +0530

    day twenty three problem 2

--- CodeForce/BeautifulMatrix.cpp
+++ CodeForce/BeautifulMatrix.cpp
@@ -3,9 +3,10 @@ using namespace std;
 int main(){
     int m[5][5];
     for(int i=0;i<5;i++){
-        for(int j = 5; j < 5; j++){
+        for(int j = 0; j < 5; j++){
             cin>>m[i][j];
         }
     }
+    
     
 }
--- CodeForce/tempCodeRunnerFile.cpp
+++ CodeForce/tempCodeRunnerFile.cpp
@@ -1,19 +1,3 @@
-//https://codeforces.com/problemset/problem/158/A
-
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int numParticipant, finisherPlace;
-    cin>>numParticipant>>finisherPlace;
-    int count = 0;
-    int arr[numParticipant];
-    for(int i=0;i<numParticipant;i++){
-        // int contestant;
-        // cin>>contestant;
-       cin>> arr[i];
-        //if(contestant>finisherPlace) count++;
-    }
-    for(int i=0;i<numParticipant;i++){
+   cout << 2;
+            i++;
         
-        if(arr[i]>=arr[finisherPlace-1])
-        {
