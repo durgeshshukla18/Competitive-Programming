76394371a5c0d0497fec1b8642b686a153fdc647
Durgesh Shukla <thegdshukla@gmail.com>
2024-05-06 23:30:10 +0530

    day twenty four

---
+++ CodeForce/YoungPhysicist.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 0; i<n; i++){
+        int a[n], b[n], c[n];
+        for(int j = 0; j<3; j++){
+            cin>>arr[j];
+        }
+    }
+}
