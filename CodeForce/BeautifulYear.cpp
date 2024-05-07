d05ef883cc0c51e6cb278d13b48c059a6ccf76ae
Durgesh Shukla <thegdshukla@gmail.com>
2024-05-07 20:41:15 +0530

    day twenty five

--- CodeForce/BeautifulYear.cpp
+++ CodeForce/BeautifulYear.cpp
@@ -1,31 +1,12 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int check(int year){
-    string x = to_string(year);
-    int count = 0;
-    for(int i=0;i<4;i++){
-        while(i<3){
-            if(x[i]==x[i+1]){
-                count++;
-            }
-        }
-        if(x[0]==x[3]){
-            count++;
-        }
-    }
-    if(count>0){
-        return true;
-    }
-    while(check(year)!=true){
-        year++;
-    }
-    cout<<year;
-}
 int main(){
-    int y;
-    cin>>y;
-    int count = 0;
-    int ycount = 0;
-    string a = to_string(y);
-    check(y);
+    int year;
+    cin>>year;
+    string to_string(int year);
+    for(int i=0;i<4;i++){
+        for(int j=1;j<4;j++){
+            
+        }
+    }
 }
---
+++ CodeForce/Magnet.cpp
@@ -0,0 +1,11 @@
+// https://codeforces.com/problemset/problem/344/A
+
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int i = 0 ; i < t; i++){
+        
+    }
+}
