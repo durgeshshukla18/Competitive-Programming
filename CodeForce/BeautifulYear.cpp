fe3098730c17af8b71f449b95b4d4c2b0cefb0ad
Durgesh Shukla <thegdshukla@gmail.com>
2024-05-08 17:58:02 +0530

    day twenty six problem 1

--- CodeForce/BeautifulYear.cpp
+++ CodeForce/BeautifulYear.cpp
@@ -1,12 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
+int check(int y){
+    
+}
 int main(){
     int year;
     cin>>year;
+    int count = 0;
     string to_string(int year);
-    for(int i=0;i<4;i++){
-        for(int j=1;j<4;j++){
-            
-        }
-    }
+    
+    
 }
