a0d5c39e388830f28e11287a616709b4258314dc
Durgesh Shukla <thegdshukla@gmail.com>
2024-05-09 23:50:43 +0530

    day twenty seven

---
+++ CodeForce/George_and_Accommodation.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    int count = 0;
+    for(int i=0; i<t; i++){
+        int a,b;
+        cin>>a>>b;
+        if(b-a>=2) count++;
+    }
+    cout<<count<<endl;
+}
