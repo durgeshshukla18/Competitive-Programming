ab54472ec68f0b9e763db3a7f8631cb160fb4099
Durgesh Shukla <thegdshukla@gmail.com>
2024-05-10 12:26:38 +0530

    day twenty eight problem 1

--- CodeForce/Magnet.cpp
+++ CodeForce/Magnet.cpp
@@ -5,7 +5,17 @@ using namespace std;
 int main(){
     int t;
     cin>>t;
+    int count = 1;
+    int arr[t];
     for(int i = 0 ; i < t; i++){
-        
+        cin>>arr[i];
     }
+    for(int i = 0 ; i < t; i++){
+        if(i<t-1){
+            if(arr[i]!=arr[i+1]){
+                count++;
+            }
+        }
+    }
+    cout<<count;
 }
