51c46a984a7d933ba65e36e837b6e7d0d052f62a
Durgesh Shukla <thegdshukla@gmail.com>
2024-05-13 23:49:48 +0530

    day thirty one

---
+++ CodeForce/PoliceRecruits.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    int count = 0;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+
+    }
+    for(int j=0; j<n; j++){
+        if(arr[j]>0){
+            
+        }
+    }
+}
