e4422d2408885cd974bbfcbcce248a8d2b58a4ae
Durgesh Shukla <thegdshukla@gmail.com>
2024-05-17 22:55:10 +0530

    day thirty four problem three

---
+++ CodeForce/Sum.cpp
@@ -0,0 +1,20 @@
+// https://codeforces.com/problemset/problem/1742/A
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int i = 0; i < t; i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if( (a+b) == c || (b+c) == a || (a+c) == b)
+        {
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
