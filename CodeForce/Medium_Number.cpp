8241d14faf81388d34ad95f52d9c664c6eed6a0d
Durgesh Shukla <thegdshukla@gmail.com>
2024-05-23 23:30:13 +0530

    day fourty

---
+++ CodeForce/Medium_Number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if((a > b && b > c) || (a < b && b < c)){
+            cout<<b<<endl;
+        }
+        else if((b > a && a > c) || (b < a && a < c)){
+            cout<<a<<endl;
+        }
+        else if((a > c && c > b) || (a < c && c < b)){
+            cout<<c<<endl;
+        }
+    }
+}
